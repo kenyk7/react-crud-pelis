923f478c12290b433a09a80a4625e10d24bdefa6
Kenyk Romero <kenykalin@gmail.com>
2019-04-09 11:47:42 -0500

    fix jsx lin

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -3,10 +3,10 @@ import React from "react";
 function Header() {
   return (
     <header className="header-page">
-      <a href="#">
+      <a href="/">
         <img className="logo" src="https://picsum.photos/90/40" alt="logo" />
       </a>
-      <a href="#" className="nav-profile">
+      <a href="/" className="nav-profile">
         <img src="https://picsum.photos/40" alt="logo" />
         <span>Admin</span>
       </a>
--- src/layout/Sidebar.js
+++ src/layout/Sidebar.js
@@ -3,14 +3,14 @@ import React from "react";
 function Header() {
   return (
     <nav className="nav-sidebar">
-      <a href="#">Dashboard</a>
-      <a href="#" className="active">
+      <a href="/">Dashboard</a>
+      <a href="/" className="active">
         Películas
       </a>
-      <a href="#">Turnos</a>
-      <a href="#">Administradores</a>
-      <a href="#">Perfil</a>
-      <a href="#">Cerrar sesión</a>
+      <a href="/">Turnos</a>
+      <a href="/">Administradores</a>
+      <a href="/">Perfil</a>
+      <a href="/">Cerrar sesión</a>
     </nav>
   );
 }
